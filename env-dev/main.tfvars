e5826cd31ab886d32606ffeabdcb721ff675ad7b
meghasyam1997 <meghasyam01@gmail.com>
2023-06-20 11:43:41 +0530

    state file to parameter-store

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters=[
+  {name= "dev.frontend.catalogue_url",value= "http://catalogue-dev.msdevops72.online:8080/"},
+  {name= "dev.frontend.cart_url",value= "http://cart-dev.msdevops72.online:8080/"}
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].name
   type  = "SecureString"
-  value = "Hello World"
   key_id = "f2e4231a-4405-4fcc-9d18-bb7c5e81348f"
 }
