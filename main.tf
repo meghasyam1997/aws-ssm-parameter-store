527dca21661729dcb59122b45b18023c856bba1f
meghasyam1997 <meghasyam01@gmail.com>
2023-06-21 11:32:01 +0530

    state file to parameter-store

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
-  value = var.parameters[count.index].name
+  value = var.parameters[count.index].value
   type  = "SecureString"
   key_id = "f2e4231a-4405-4fcc-9d18-bb7c5e81348f"
 }
