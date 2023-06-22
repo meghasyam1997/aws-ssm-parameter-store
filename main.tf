1258b1f939e8ad5d1212741900b15dbf98bed048
meghasyam1997 <meghasyam01@gmail.com>
2023-06-22 10:56:15 +0530

    aws instance profile

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
-resource "aws_ssm_parameter" "parameter" {
+resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
+  key_id = "f2e4231a-4405-4fcc-9d18-bb7c5e81348f"
+}
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
   name  = var.parameters[count.index].name
   value = var.parameters[count.index].value
   type  = "SecureString"
